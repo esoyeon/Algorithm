d492f2ee81517d034cf803ac0bb26c9b9992fb40
esoyeon <nplsoyeon@gmail.com>
2021-02-13 18:11:52 +0900

    효율적인 코드 추가

--- Hacker_ClimbingtheLeaderboard.py
+++ Hacker_ClimbingtheLeaderboard.py
@@ -16,51 +16,63 @@ import sys
 
 from collections import defaultdict
 
-# p보다 크면서 가장 가까운 값 찾기 / 이진탐색 응용 
-def search_bigger(ranked, p):
-    start, end = 0, len(ranked)
-    while start <= end:
-        mid = (start + end) // 2
-        # 보드 값들보다 작으면 보드 중 마지막값
-        if ranked[-1] >= p:
-            return ranked[-1]
+# # p보다 크면서 가장 가까운 값 찾기 / 이진탐색 응용 
+# def search_bigger(ranked, p):
+#     start, end = 0, len(ranked)
+#     while start <= end:
+#         mid = (start + end) // 2
+#         # 보드 값들보다 작으면 보드 중 마지막값
+#         if ranked[-1] >= p:
+#             return ranked[-1]
 
-        # 리턴 조건 
-        if (ranked[mid] >= p and p >= ranked[mid+1]):
-            return ranked[mid]
+#         # 리턴 조건 
+#         if (ranked[mid] >= p and p >= ranked[mid+1]):
+#             return ranked[mid]
 
-        # 출발값 조정
-        elif ranked[mid] > p:
-            start = mid+1
-        # 마지막 값 조정
-        else: 
-            end = mid -1        
+#         # 출발값 조정
+#         elif ranked[mid] > p:
+#             start = mid+1
+#         # 마지막 값 조정
+#         else: 
+#             end = mid -1        
     
+# def climbingLeaderboard(ranked, player):
+#     board = defaultdict(lambda: len(board)+1)
+#     result = []
+#     # board 생성
+#     for r in ranked:
+#         if r not in board:
+#             board[r]
+#     print(board)
+
+#     # player 검사 
+#     for p in player:
+#         # 최댓값보다 크면 1순위
+#         if p >= ranked[0]:
+#             result.append(1)
+
+#         # 보드에 있는 값이면 기존 순위
+#         elif p in board:
+#             result.append(board[p])
+
+#         # 보드값들 중 가장 가까우면서 p보다 큰 값 찾아서 + 1
+#         else:
+#             key = search_bigger(ranked, p)
+#             new_rank = board[key] + 1
+#             result.append(new_rank)
+    
+#     return result
+
+
+## 다른 사람코드 -> 효율적! 
 def climbingLeaderboard(ranked, player):
-    board = defaultdict(lambda: len(board)+1)
+    leaderboard = sorted(set(ranked), reverse = True)
+    l = len(leaderboard)
     result = []
-    # board 생성
-    for r in ranked:
-        if r not in board:
-            board[r]
-    print(board)
-
-    # player 검사 
     for p in player:
-        # 최댓값보다 크면 1순위
-        if p >= ranked[0]:
-            result.append(1)
-
-        # 보드에 있는 값이면 기존 순위
-        elif p in board:
-            result.append(board[p])
-
-        # 보드값들 중 가장 가까우면서 p보다 큰 값 찾아서 + 1
-        else:
-            key = search_bigger(ranked, p)
-            new_rank = board[key] + 1
-            result.append(new_rank)
-    
+        while (l > 0) and (p >= leaderboard[l-1]):
+            l -= 1
+        result.append(l+1)
     return result
 
 ranked = [100, 100, 50, 40, 40, 20, 10]
